79d0c1caa2e56e43fbcc2ef72aebe63fc7cc7fdf
lizhaoxx <lizhao15431230@qq.com>
2016-03-05 11:45:11 +0800

    适配iOS6中非扁平化的导航栏的样式 1、通过 [UINavigationBar appearance] 获取的 UINavigationBar 对象，设置导航的样式

--- PaPaWeiBo/Classes/Main(主要)/Controller/LZNavigationController.m
+++ PaPaWeiBo/Classes/Main(主要)/Controller/LZNavigationController.m
@@ -10,33 +10,54 @@
 
 @implementation LZNavigationController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
++(void)initialize {
+    // 设置 NavigationBar 的样式
+    [self setupNavigationBarTheme];
+    
+    // 设置 UIBarButton 的样式
+    [self setupUIBarButtonTheme];
+}
 
-    // 设置整个项目 UIBarButton 的样式
-    [self setupUIBarButtonStyle];
+// 设置 NavigationBar 的样式
++ (void)setupNavigationBarTheme {
+    UINavigationBar *appearance = [UINavigationBar appearance];
+    
+    if (iOS6) {
+        [appearance setBackgroundImage:[UIImage imageNamed:@"navigationbar_background"] forBarMetrics:UIBarMetricsDefault];
+    }
+    
+    NSMutableDictionary *textAttr = [NSMutableDictionary dictionary];
+    textAttr[NSFontAttributeName] = [UIFont boldSystemFontOfSize:20];
+    textAttr[UITextAttributeTextShadowOffset] = [NSValue valueWithUIOffset:UIOffsetZero];// 取消阴影效果，在ios6中有阴影的效果
+    [appearance setTitleTextAttributes:textAttr];
 }
 
 /**
  *  设置整个项目 UIBarButton 的样式
  *  通过 appearance 对象能修改整个项目中所有的 UIBarButton的样式
  */
-- (void)setupUIBarButtonStyle {
++ (void)setupUIBarButtonTheme {
 
     UIBarButtonItem *appearance = [UIBarButtonItem appearance];
     
+    /** 设置文字的样式 */
     NSMutableDictionary *textAttr = [NSMutableDictionary dictionary];
     textAttr[NSFontAttributeName] = [UIFont systemFontOfSize:15];
     textAttr[NSForegroundColorAttributeName] = [UIColor orangeColor];
+    textAttr[UITextAttributeTextShadowOffset] = [NSValue valueWithUIOffset:UIOffsetZero];
     [appearance setTitleTextAttributes:textAttr forState:UIControlStateNormal];
     
-    NSMutableDictionary *highTextAttr = [NSMutableDictionary dictionary];
+    NSMutableDictionary *highTextAttr = [NSMutableDictionary dictionaryWithDictionary:textAttr];
     highTextAttr[NSForegroundColorAttributeName] = [UIColor blackColor];
     [appearance setTitleTextAttributes:highTextAttr forState:UIControlStateHighlighted];
     
-    NSMutableDictionary *disableTextAttr = [NSMutableDictionary dictionary];
+    NSMutableDictionary *disableTextAttr = [NSMutableDictionary dictionaryWithDictionary:textAttr];
     disableTextAttr[NSForegroundColorAttributeName] = [UIColor lightGrayColor];
     [appearance setTitleTextAttributes:disableTextAttr forState:UIControlStateDisabled];
+    
+    /** 设置背景的图片 */
+    // 为了让某个按钮的背景消失，设置一直完全透明的背景图片
+    [appearance setBackgroundImage:[UIImage imageNamed:@"navigationbar_button_background"] forState:UIControlStateNormal barMetrics:UIBarMetricsDefault];
 }
 
 /**
